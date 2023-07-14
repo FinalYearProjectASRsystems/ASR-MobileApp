bfe7a623fb24aa7c81929f471550127e664f28fc
Carmpeli <109292009+Carmpeli@users.noreply.github.com>
2023-07-13 21:57:15 -0700

    Friday 13th
    Attempted update to multilingual support and transcription history filename list before card component

--- lib/main.dart
+++ lib/main.dart
@@ -246,8 +246,125 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-
 class TranscriptionHistoryPage extends StatelessWidget {
+  final List<String> filenames = [
+    'Transcription 1',
+    'Transcription 2',
+    'Transcription 3',
+    // Add more filenames as needed
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transcription History'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Navigate back to the home page
+          },
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        width: double.infinity,
+        color: Colors.black, // Set the background color to black for high contrast
+        child: ListView.builder(
+          itemCount: filenames.length,
+          itemBuilder: (BuildContext context, int index) {
+            final filename = filenames[index];
+            return InkWell(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Container(
+                        width: double.maxFinite,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              filename,
+                              style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black, // Set the text color to black for high contrast
+                              ),
+                            ),
+                            SizedBox(height: 16.0),
+                            IconButton(
+                              icon: Icon(Icons.play_arrow),
+                              onPressed: () {
+                                // Add audio playback logic here
+                              },
+                              color: Colors.black, // Set the icon color to black for high contrast
+                            ),
+                            SizedBox(height: 16.0),
+                            TextField(
+                              readOnly: true,
+                              style: TextStyle(
+                                color: Colors.black, // Set the text color to black for high contrast
+                              ),
+                              decoration: InputDecoration(
+                                labelText: 'Your text here',
+                                labelStyle: TextStyle(
+                                  color: Colors.black, // Set the label color to black for high contrast
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.black, // Set the border color to black for high contrast
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 16.0),
+                            ElevatedButton(
+                              onPressed: () {
+                                // Handle translation logic here
+                              },
+                              child: Text('Translate to English'),
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.black, // Set the button background color to black for high contrast
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Container(
+                margin: EdgeInsets.only(bottom: 16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white, // Set the container background color to white for high contrast
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    filename,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black, // Set the text color to black for high contrast
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+
+/*class TranscriptionHistoryPage extends StatelessWidget {
   final List<String> filenames = [
     'Transcription 1',
     'Transcription 2',
@@ -351,7 +468,7 @@ final assetsAudioPlayer = AssetsAudioPlayer();
 }
 
 
-/*class AudioPlayerWidget extends StatefulWidget {
+class AudioPlayerWidget extends StatefulWidget {
   final String audioPath;
 
   AudioPlayerWidget({required this.audioPath});
